4b05fa75b11ece68c04b10e535263aed9562a5ca
Michael Bigelow <procureeminence@gmail.com>
2020-07-31 20:52:29 -0400

    feat: Finished auth code

--- backend/src/auth/auth.py
+++ backend/src/auth/auth.py
@@ -10,10 +10,7 @@ ALGORITHMS = ['RS256']
 API_AUDIENCE = 'coffee-api'
 
 ## AuthError Exception
-'''
-AuthError Exception
-A standardized way to communicate auth failure modes
-'''
+
 class AuthError(Exception):
 	def __init__(self, error, status_code):
 		self.error = error
@@ -124,16 +121,6 @@ def verify_decode_jwt(token):
 				'description': 'Unable to find the appropriate key.'
 			}, 400)
 
-'''
-@TODO implement @requires_auth(permission) decorator method
-	@INPUTS
-		permission: string permission (i.e. 'post:drink')
-
-	it should use the get_token_auth_header method to get the token
-	it should use the verify_decode_jwt method to decode the jwt
-	it should use the check_permissions method validate claims and check the requested permission
-	return the decorator which passes the decoded payload to the decorated method
-'''
 def requires_auth(permission=''):
 	def requires_auth_decorator(f):
 		@wraps(f)
